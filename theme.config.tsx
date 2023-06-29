8a3009fb85d624210b9257709b25287a32d42a3d
Taiga Saito <me@taigasaito.org>
2023-06-29 12:40:51 +0900

    update config

--- theme.config.tsx
+++ theme.config.tsx
@@ -6,6 +6,12 @@ const config: DocsThemeConfig = {
   footer: {
     text: '©︎2023 齋藤 大雅',
   },
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - 地学部マニュアル'
+    }
+  },
 }
 
 export default config
