ce0729be4a4aa38a15aa58d3dd42cf914c4ae8f4
Taiga Saito <me@taigasaito.org>
2023-06-29 18:48:43 +0900

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,28 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>地学部マニュアル</span>,
+  logo: <span style={{ fontWeight: "600", fontSize: "1.2rem" }}>地学部マニュアル</span>,
+  search: {
+    placeholder: 'マニュアルを検索...',
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
   footer: {
     text: '©︎2023 齋藤 大雅',
   },
-
+  themeSwitch: {
+    useOptions() {
+      return {
+        light: 'ライト',
+        dark: 'ダーク',
+        system: '自動'
+      }
+    }
+  },
   useNextSeoProps() {
     return {
       titleTemplate: '%s - 地学部マニュアル'
